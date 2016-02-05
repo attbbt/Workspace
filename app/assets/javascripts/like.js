b94ca35742e735385bd8d2c8a4f221ce52fb7c70
srinivasknss <srinivasknss@gmail.com>
2016-02-05 08:12:53 -0600

    fixed nabbar for mobile devices

--- app/assets/javascripts/like.js
+++ app/assets/javascripts/like.js
@@ -5,3 +5,9 @@ function adjust_body_offset() {
 $(window).resize(adjust_body_offset);
 
 $(document).ready(adjust_body_offset);
+
+$(window).load(adjust_body_offset);
+
+var shiftWindow = function() { scrollBy(0, -50) };
+if (location.hash) shiftWindow();
+window.addEventListener("hashchange", shiftWindow);

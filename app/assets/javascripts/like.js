ef51df47aba4f85711dd3e93cef8ef7bc9b74cc5
srinivasknss <srinivasknss@gmail.com>
2016-02-05 12:37:35 -0600

    add navbar collapse

--- app/assets/javascripts/like.js
+++ app/assets/javascripts/like.js
@@ -7,7 +7,3 @@ $(window).resize(adjust_body_offset);
 $(document).ready(adjust_body_offset);
 
 $(window).load(adjust_body_offset);
-
-var shiftWindow = function() { scrollBy(0, -50) };
-if (location.hash) shiftWindow();
-window.addEventListener("hashchange", shiftWindow);
